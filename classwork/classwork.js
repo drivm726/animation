cc3b7a565f5ca2a14b99c27706d4e503a8420813
drivm726 <drivm726@newschool.edu>
2016-02-16 18:05:21 -0500

    Melissa-Bouncing Balls
    completed!

--- classwork/classwork.js
+++ classwork/classwork.js
@@ -1,21 +1,35 @@
 /*
   Classwork
 */
-var ballX = 0;
-var speed = 3;
-
+var circleY = 0;
+var speed1 = -4;
+var circleX = 0;
+var speed2 = -4;
 function setup(){
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw(){
-  background("white");
-  ellipse(ballX, height/2, 50,50);
+background("white");
 
-  if (ballX >= width){
-    speed = -3;
+if (mouseIsPressed){
+fill(204,204,255);
+
+}
+
+
+  ellipse(circleX, height/2, 50,50);
+  if (circleX >= width || circleX <=0){
+    speed1 = -speed1;
   }
 
-  ballX = ballX + speed;
+  circleX = circleX + speed1;
+
+  ellipse (width/2, circleY, 50, 50);
+  if (circleY >= height || circleY <= 0){
+    speed2 = -speed2;
+  }
+  circleY = circleY + speed2;
+
 
 }
