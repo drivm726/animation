da48e4020968876069bb9a511c317ece12bfcf38
drivm726 <drivm726@newschool.edu>
2016-03-01 14:38:10 -0500

    Animation HW

--- homework/homework.js
+++ homework/homework.js
@@ -2,14 +2,38 @@
   Homework
 */
 
-//declare your variables
-
+var circleX = 0;
 function setup(){
-  createCanvas(windowWidth,windowHeight);
-
-  //set things up
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw(){
-  //draw
+  background("skyblue");
+
+  if (circleX > windowWidth/2){
+}
+  if(mouseX > windowWidth/2){
+  background("black");
+  fill("skyblue");
+
+  if (mouseIsPressed){
+  fill(204,204,255);
+
+  }
+
+  }
+
+  noStroke();
+  fill("gold");
+  ellipse(mouseX, mouseY, 50, 50);
+
+
+
+
+
+//if the circle reaches the end of the window,
+//reset it to 0 to start at the beginning of the frame
+  if (circleX >= windowWidth){
+    circleX = 0
+  }
 }
